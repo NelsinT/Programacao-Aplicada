2e4ce209ce62939cd106b6536d2a4ef931cd8ba3
NelsinT <nelsoncunha2101@gmail.com>
2026-03-10 17:01:46 +0000

    aa

--- modelo/Utilizador.java
+++ modelo/Utilizador.java
@@ -1,11 +1,13 @@
-public class utilizador {
+package modelo;
+
+public class Utilizador {
     private String nome;
     private String username;
     private String password;
     private String email;
     private boolean ativo;
 
-    public utilizador(String aNome, String aUsername, String aPassword, String aEmail) {
+    public Utilizador(String aNome, String aUsername, String aPassword, String aEmail) {
 
         nome = aNome;
         username = aUsername;
