03f7b9b5f162298d8a10d4e384526f80fa9d3267
NelsinT <nelsoncunha2101@gmail.com>
2026-03-10 16:38:34 +0000

    Update utilizador.java

--- utilizador.java
+++ utilizador.java
@@ -1,3 +1,19 @@
 public class utilizador {
-    
+    private String nome;
+    private String username;
+    private String password;
+    private boolean estado;
+    private String email;
+    private String tipo;
+
+    public utilizador(String aNome, String aUsername, String aPassword, Boolean aEstado, String aEmail) {
+
+        nome = aNome;
+        username = aUsername;
+        password = aPassword;
+        estado = aEstado;
+        email = aEmail;
+
+    }
+
 }
