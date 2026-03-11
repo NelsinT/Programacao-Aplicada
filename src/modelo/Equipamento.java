ad8e3d481749f6abbfef376bdf69342fae161621
Unlucky1970 <dinisramalheira@gmail.com>
2026-03-11 10:16:20 +0000

    Construtores

--- src/modelo/Equipamento.java
+++ src/modelo/Equipamento.java
@@ -10,4 +10,43 @@ public class Equipamento {
     private String lote;
     private LocalDate dataSubmissaoPedido;
     private LocalDate dataReparacao;
+
+    public Equipamento(String aMarca, String aModelo, int aCodigoSku, LocalDateTime aDataFabrico, String aLote, LocalDate aDataSubmissaoPedido, LocalDate aDataReparacao) {
+        marca = aMarca;
+        modelo = aModelo;
+        codigoSku = aCodigoSku;
+        dataFabrico = aDataFabrico;
+        lote = aLote;
+        dataSubmissaoPedido = aDataSubmissaoPedido;
+        dataReparacao = aDataReparacao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getCodigoSku() {
+        return codigoSku;
+    }
+
+    public LocalDateTime getDataFabrico() {
+        return dataFabrico;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public LocalDate getDataSubmissaoPedido() {
+        return dataSubmissaoPedido;
+    }
+
+    public LocalDate getDataReparacao() {
+        return dataReparacao;
+    }
+
 }
--- src/modelo/Reparacao.java
+++ src/modelo/Reparacao.java
@@ -14,4 +14,53 @@ public class Reparacao {
     private String observacoes;
     private List<TesteOperacionalidade> testes;
     private Map<Peca, Integer> pecasUtilizadas;
+
+    public Reparacao(String aNumeroReparacao, LocalDateTime aDataCriacao, LocalDateTime aDataFimReparacao, long aTempoDecorrido, double aCusto, int aEstado, String aObservacoes, List<TesteOperacionalidade> aTestes, Map<Peca, Integer> aPecasUtilizadas) {
+        numeroReparacao = aNumeroReparacao;
+        dataCriacao = aDataCriacao;
+        dataFimReparacao = aDataFimReparacao;
+        tempoDecorrido = aTempoDecorrido;
+        custo = aCusto;
+        estado = aEstado;
+        observacoes = aObservacoes;
+        testes = aTestes;
+        pecasUtilizadas = aPecasUtilizadas;
+    }
+
+    public String getNumeroReparacao() {
+        return numeroReparacao;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public LocalDateTime getDataFimReparacao() {
+        return dataFimReparacao;
+    }
+
+    public long getTempoDecorrido() {
+        return tempoDecorrido;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public List<TesteOperacionalidade> getTestes() {
+        return testes;
+    }
+
+    public Map<Peca, Integer> getPecasUtilizadas() {
+        return pecasUtilizadas;
+    }
+
 }
