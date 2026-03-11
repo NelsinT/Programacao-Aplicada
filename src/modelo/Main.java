e6abdfee3920353180708a6ca742d887d7fe5c55
Unlucky1970 <dinisramalheira@gmail.com>
2026-03-11 10:39:35 +0000

    Main, olá e adeus

---
+++ src/modelo/Main.java
@@ -0,0 +1,9 @@
+package src.modelo;
+
+public class Main {
+    public static void main() {
+        Utilizador u = new Utilizador("John Doe", "johndoe", "password123", "johndoe@example.com");
+        System.out.println("Bem-vindo " + u.getNome());
+        System.out.println("Adeus, " + u.getNome());
+    }
+}
